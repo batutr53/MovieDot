03ae25cf4038ae06bde2562b4cb130d3d661e677
Batuhan Türk <batuhanturk34@gmail.com>
2022-03-28 16:40:44 +0300

    Jwt

--- MovieDot.API/Program.cs
+++ MovieDot.API/Program.cs
@@ -1,13 +1,35 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieDot.API.Filters;
+using MovieDot.API.Middlewares;
 using MovieDot.API.Modules;
 using MovieDot.Repository;
+using MovieDot.Service.Mapping;
+using MovieDot.Shared;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
+using System.Text;
 
+//cors
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://localhost:44387",
+                                              "https://localhost:4200",
+                                              "http://localhost:4200")
+                                                      .AllowAnyHeader()
+                                                      .AllowAnyMethod();
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -17,8 +39,32 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
 builder.Services.AddSwaggerGen();
 
+//jwt
+var appSettings = builder.Configuration.GetSection("appSettings");
+builder.Services.Configure<AppSettings>(appSettings);
+var appSettingsB = appSettings.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettingsB.Secret);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => {
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddScoped(typeof(NotFoundFilter<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 //Db
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
@@ -40,7 +86,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCustomException();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
